62ab3b81db92e0815ad8c7be38a3bd626156b72d
zombiepig333 <zombiepig333@gmail.com>
2014-07-15 18:01:43 -0700

    Project setup.

---
+++ src/main/java/com/zpig333/runesofwizardry/RunesOfWizardry.java
@@ -0,0 +1,13 @@
+package com.zpig333.runesofwizardry;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by zombiepig333 on 15-07-14.
+ * <p/>
+ * Licensed under the GPLv3
+ */
+@Mod(modid = "RunesWiz", name = "Runes of Wizardry", version = "Dev")
+
+public class RunesOfWizardry {
+}
