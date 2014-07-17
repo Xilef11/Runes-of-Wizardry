8d1f94ffd394a49b022b8e4c7e187eca03507918
zombiepig333 <zombiepig333@gmail.com>
2014-07-17 16:39:51 -0700

    God, I am bad at textures.

--- src/main/java/com/zpig333/runesofwizardry/core/WizardryRegistry.java
+++ src/main/java/com/zpig333/runesofwizardry/core/WizardryRegistry.java
@@ -1,6 +1,7 @@
 package com.zpig333.runesofwizardry.core;
 
 import com.zpig333.runesofwizardry.item.ItemPestle;
+import com.zpig333.runesofwizardry.item.ItemPlantBalls;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -16,17 +17,27 @@ import net.minecraftforge.oredict.OreDictionary;
 public class WizardryRegistry {
 
     public static Item pestle;
+    public static Item plant_balls;
 
     public static void initItems(){
 
         pestle = new ItemPestle().setUnlocalizedName("pestle_wizardry");
         GameRegistry.registerItem(pestle, "pestle_wizardry");
+
+        plant_balls = new ItemPlantBalls().setUnlocalizedName("plant_balls");
+        GameRegistry.registerItem(plant_balls, "plant_balls");
     }
 
     public static void initCrafting(){
 
         //Stuuuuuuupid mojang had to make 2 different leaves, didn't they?
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.apple), new ItemStack(Blocks.leaves, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(pestle, 1, OreDictionary.WILDCARD_VALUE));
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.apple), new ItemStack(Blocks.leaves2, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(pestle, 1, OreDictionary.WILDCARD_VALUE));
+        GameRegistry.addShapelessRecipe(new ItemStack(plant_balls, 1, 1), new ItemStack(Blocks.leaves, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(pestle, 1, OreDictionary.WILDCARD_VALUE));
+        GameRegistry.addShapelessRecipe(new ItemStack(plant_balls, 1, 1), new ItemStack(Blocks.leaves2, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(pestle, 1, OreDictionary.WILDCARD_VALUE));
+
+        //Craft the small plant balls into larger ones!!
+        GameRegistry.addRecipe(new ItemStack(plant_balls, 1, 0), new Object[]{
+                "XXX", "XXX", "XXX", 'X', new ItemStack(plant_balls, 1, 1)
+        });
+
     }
 }
--- src/main/java/com/zpig333/runesofwizardry/item/ItemPlantBalls.java
+++ src/main/java/com/zpig333/runesofwizardry/item/ItemPlantBalls.java
@@ -1,7 +1,15 @@
 package com.zpig333.runesofwizardry.item;
 
+import com.zpig333.runesofwizardry.core.References;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+import java.util.List;
 
 /**
  * Created by zombiepig333 on 17-07-14.
@@ -10,9 +18,38 @@ import net.minecraft.item.Item;
  */
 public class ItemPlantBalls extends Item {
 
+    private IIcon[] icons;
+
+
     public ItemPlantBalls(){
         super();
         this.setCreativeTab(CreativeTabs.tabMaterials);
+        this.setHasSubtypes(true);
     }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack){
+        int meta = itemStack.getItemDamage();
+        return super.getUnlocalizedName() + "." + meta;
+    }
+
+    @Override
+    public IIcon getIconFromDamage(int meta){
+        return icons[meta];
+    }
+
+    @Override
+    public void getSubItems(Item item, CreativeTabs tabs, List list){
+        list.add(new ItemStack(item, 1, 0));
+        list.add(new ItemStack(item, 1, 1));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister ireg){
+        icons = new IIcon[2];
+        icons[0] = ireg.registerIcon(References.texture_path + "plantball");
+        icons[1] = ireg.registerIcon(References.texture_path + "plantball_small");
+    }
+
 
 }
